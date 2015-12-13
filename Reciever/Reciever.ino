451db922c48cbe0783169d0bf11a46b0165ca78f
Bram Korsten <exentory@gmail.com>
2015-12-13 20:02:47 +0100

    Version 1.0.1
    Cleaned up the code

--- Reciever/Reciever.ino
+++ Reciever/Reciever.ino
@@ -1,54 +1,59 @@
 #include <VirtualWire.h>
 
 /*
-SimpleReceive
-This sketch displays text strings received using VirtualWire
-Connect the Receiver data pin to Arduino pin 11
-*/
+ * Reciever module (slave)
+ * Kekke FestiBracelet
+ * This program recieves incoming commands using the VirtualWire Library. 
+ * The code handles the request onboard, and sends the correct information to the leds.
+ */
+
 byte message[VW_MAX_MESSAGE_LEN]; // a buffer to store the incoming messages
 byte messageLength = VW_MAX_MESSAGE_LEN; // the size of the message
-
-bool ledon = false;
+// set led IO ports
 int red = 7;
 int green = 8;
 int blue = 9;
-
+// set default blinkspeed and color
 int blinkinterval = 200;
 String blinkcolor = "";
 
-void setup()
+void setup() // code to run once
 {
+  // set the led IO ports as outputs
   pinMode(red, OUTPUT);
   pinMode(green, OUTPUT);
   pinMode(blue, OUTPUT);
-  pinMode(4, OUTPUT);
-  digitalWrite(4, HIGH);
+  pinMode(4, OUTPUT); // digital pin 4 is used to power the radio module
+  digitalWrite(4, HIGH); // power digital pin 4
   // Initialize the IO and ISR
   vw_setup(2000); // Bits per sec
-  vw_set_rx_pin(3);
+  vw_set_rx_pin(3); // set the reciever pin to digital pin 3
   vw_rx_start(); // Start the receiver
-  Serial.begin(9600);
-  Serial.println("Reciever is ready");
+  Serial.begin(9600); // Start the serial monitor for debugging
+  Serial.println("Slave ready!");
 }
-void loop()
+void loop() // code to run at all times
 {
-  //blinker(blinkcolor);
-  if (vw_get_message(message, &messageLength)) // Non-blocking
+  if (vw_get_message(message, &messageLength)) // Non-blocking, if the reciever gets a message, run the code
   {
 
-    //strcpy((char *)message, "");
-    //String messagestring = String((char *)message);
-
-    Serial.print("Message Recieved: ");
+    Serial.print("Message Recieved: "); // this code prints out the message recieved for debugging
     for (int i = 0; i < messageLength; i++)
     {
       Serial.write(message[i]);
     }
     Serial.println();
 
-    if ((String((char *)message).substring(0,2)) == "00") {
+    /*
+     * The message that should be recieved is always five digits long
+     * the first 2 digits are used for achieving diferent results
+     *  "00" - Set Color
+     *  "10" - Set Animation Speed
+     *  "20" - Set Animation Type
+     */
+    if ((String((char *)message).substring(0,2)) == "00") { // check the contents of the recieved message for type
       
-      if ((String((char *)message)) == "00red") {
+      if ((String((char *)message)) == "00red") { // if message reads [color], set this color
         digitalWrite(red, HIGH);
         digitalWrite(green, LOW);
         digitalWrite(blue, LOW);
@@ -65,21 +70,13 @@ void loop()
       }
       
     }
-    if ((String((char *)message).substring(0,2)) == "10") {
-      
-        blinkinterval = (String((char *)message).substring(2,5)).toInt();
-        //Serial.println(blinkinterval);
-        
+    if ((String((char *)message).substring(0,2)) == "10") { // if the content is "10", set the speed
+      /*
+       * unfinished code
+       * blinkinterval = (String((char *)message).substring(2,5)).toInt();
+       * Serial.println(blinkinterval);
+       */      
     }
   }
 }
 
-/*void blinker(color)
-{
-  if (color == "red") {
-    digitalWrite(red, HIGH);
-    digitalWrite(green, LOW);
-    digitalWrite(blue, LOW);
-  }
-}
-*/
--- Transmitter/Transmitter.ino
+++ Transmitter/Transmitter.ino
@@ -1,74 +1,53 @@
-/*
-SimpleSend
-This sketch transmits a short text message using the VirtualWire library
-connect the Transmitter data pin to Arduino pin 12
-*/
+#include <VirtualWire.h>
 
+/*
+ * Transmitter module (master)
+ * Kekke FestiBracelet
+ * This program Transmits commands recieved trough serial using the VirtualWire Library. 
+ * The code handles the request onboard, and sends the correct commands by radio to all available slaves.
+ */
+
+// debugbuttons used for sending commands, this will be replaced by serial requests ASAP
 int buttonr = 2;
 int buttong = 3;
 int buttonb = 4;
 int potentio1 = 1;
 String blinkspeedmap;
-
+// The messages that can be send, using char arrays, which are converted to bytes by the VirtualWire Library
 char red[5] = {'0','0','r','e','d'};
 char green[5] = {'0','0','g','r','e'};
 char blue[5] = {'0','0','b','l','u'};
 char blinkspeed[5] = {'1','0','0','0','0'};
 char hello[5] = {'h','e','l','l','o'};
 
-#include <VirtualWire.h>
+
 
 void setup()
 {
   // Initialize the IO and ISR
   vw_setup(2000); // Bits per sec
-  Serial.begin(9600);
-  Serial.println("Transmitter started succesfully!");
+  Serial.begin(9600); // Start the serial monitor for debugging purposes
+  Serial.println("Master started succesfully!");
 }
 void loop()
 {
   
   if (digitalRead(buttonr) == HIGH) {
-    vw_send((uint8_t *)red, 5);
+    vw_send((uint8_t *)red, 5); // use the VirtualWire command to send the char array variables to the slaves
     vw_wait_tx(); // Wait until the whole message is gone
     Serial.println("Message Sent!");
     delay(500);
-    sendblinkspeed();
-    //delay(100);
+    sendblinkspeed(); // set a new blinkspeed based on potentiometer data
   }
-  /*if (digitalRead(buttong) == HIGH) {
-    send("00gre");
-    delay(200);
-  }
-  if (digitalRead(buttonb) == HIGH) {
-    send("00blu");
-    delay(200);
-  }
-  */
 }
-      /*void send (char *message)
-      {
-        vw_send((uint8_t *)message, strlen(message));
-        
-      }
-      */
+
 void sendblinkspeed()
 {
-  blinkspeedmap = String(map( analogRead(potentio1), 0, 1024, 100, 999) + 10000);
-  //blinkspeed[2] = (char)blinkspeedmap.substring(0,1).toInt();
-  //blinkspeed[3] = (char)blinkspeedmap.substring(1,2).toInt();
-  //blinkspeed[4] = (char)blinkspeedmap.substring(2).toInt();
-  
-  blinkspeedmap.toCharArray(blinkspeed,6);
-  
-  Serial.println(blinkspeedmap);
-  Serial.println(blinkspeed);
-    //for (int i = 0; i < 5; i = i + 1) {
-    //  Serial.println(String(blinkspeed[i]));
-    //}
-  vw_send((uint8_t *)blinkspeed, 5);
+  blinkspeedmap = String(map( analogRead(potentio1), 0, 1024, 100, 999) + 10000); // set a string containing a value between 10000 and 10999 based on the value of the potentiometer
+  blinkspeedmap.toCharArray(blinkspeed,6); // convert the created string to a char array
+  Serial.println(blinkspeedmap);// debug
+  Serial.println(blinkspeed); // debug
+  vw_send((uint8_t *)blinkspeed, 5); // send the new array
   vw_wait_tx(); // Wait until the whole message is gone
-  
-  
 }
 
